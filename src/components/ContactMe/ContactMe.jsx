d0faa08a60802b7dbdb274d47e466bbd11a60bf6
Abdallah Safar <abdallah.safar01@gmail.com>
2021-09-05 21:02:07 +0300

    feat: send email with emailjs in the contact component

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import separator from "../../images/separator.svg";
+import emailjs from "emailjs-com";
 
 export default function ContactMe() {
   const [person, setPerson] = useState({
@@ -15,6 +16,33 @@ export default function ContactMe() {
     });
   }
 
+  function sendEmail(event) {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "contact-me-portfolio",
+        event.target,
+        "user_PxDqQdoEdadBNHENhNn6p"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    setPerson({
+      name: "",
+      email: "",
+      phoneNumber: "",
+      message: "",
+    });
+  }
+
   return (
     <div className="bg-gray-250 w-full h-80 flex flex-col items-center h-auto">
       <div className="border-4 border-black p-5 w-56 h-16 flex items-center justify-center montserrat font-semibold text-2xl mt-20">
@@ -24,10 +52,12 @@ export default function ContactMe() {
         I would love to hear from you!
       </h1>
       <img src={separator} alt="separator" />
-      <form className="w-lg flex flex-col items-center">
+      <form className="w-lg flex flex-col items-center" onSubmit={sendEmail}>
         <input
           type="text"
           placeholder="ENTER YOUR NAME*"
+          name="name"
+          value={person.name}
           className="border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-10 px-2 font-medium montserrat focus:outline-none mt-20"
           onChange={(event) => handleInputTyping("name", event.target.value)}
           required
@@ -35,6 +65,8 @@ export default function ContactMe() {
         <input
           type="email"
           placeholder="ENTER YOUR EMAIL*"
+          name="email"
+          value={person.email}
           className="border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-10 px-2 font-medium montserrat focus:outline-none mt-10"
           onChange={(event) => handleInputTyping("email", event.target.value)}
           required
@@ -42,6 +74,8 @@ export default function ContactMe() {
         <input
           type="number"
           placeholder="PHONE NUMBER"
+          name="phone-number"
+          value={person.phoneNumber}
           className="border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-10 px-2 font-medium montserrat focus:outline-none mt-10"
           onChange={(event) =>
             handleInputTyping("phoneNumber", event.target.value)
@@ -50,23 +84,17 @@ export default function ContactMe() {
         <textarea
           type="text"
           placeholder="YOUR MESSAGE*"
+          name="message"
+          value={person.message}
           className="border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-32 px-2 font-medium montserrat focus:outline-none mt-10"
           onChange={(event) => handleInputTyping("message", event.target.value)}
           required
         />
-        <button
+        <input
           type="submit"
-          onClick={(event) => {
-            event.preventDefault();
-            console.log(person);
-          }}
-        >
-          <div className="flex montserrat font-semibold mt-16 mb-10">
-            <pre className="text-3xl">|   </pre>
-            <pre className="text-xl self-center">SUBMIT</pre>
-            <pre className="text-3xl">   |</pre>
-          </div>
-        </button>
+          className="border-l-4 border-r-4 border-black w-40 h-10 montserrat font-semibold mt-16 mb-10 text-lg bg-transparent cursor-pointer"
+          value="SUBMIT"
+        />
       </form>
     </div>
   );
