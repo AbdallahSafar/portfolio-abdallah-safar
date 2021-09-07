2ed6271d2cd09edd4eff27180840795d93c54f5a
Abdallah Safar <abdallah.safar01@gmail.com>
2021-09-08 01:02:03 +0300

    fix: fix margins and positions in desktop home page component

--- src/components/DesktopHomePage/DesktopHomePage.jsx
+++ src/components/DesktopHomePage/DesktopHomePage.jsx
@@ -9,30 +9,52 @@ export default function DesktopHomePage() {
   return (
     <div
       id="home"
-      className="bg-desktop-home-page w-full h-screen bg-center bg-cover py-7 relative"
+      className="bg-desktop-home-page w-full h-screen bg-cover py-7 relative"
     >
       <div className="flex justify-between w-full">
-        <div className="flex justify-end w-44">
+        <div className="flex justify-end w-32">
           <img src={logo} alt="logo" className="w-16 h-16" />
         </div>
         {/* <div className="w-72 bg-gray-200 mr-96"></div> Navbar */}
       </div>
-      <div className="flex justify-between w-full h-96 items-center">
-        <div className="ml-30 mt-20">
-          <h1 className="font-bold raleway text-3xl mb-20">Hi, I am</h1>
-          <h1 className="font-bold raleway text-6xl mb-2">Abdallah Safar</h1>
-          <h1 className="font-bold raleway text-2xl text-gray-500">
-            Front-End Web Developer
-          </h1>
+
+      <div className="mt-24 w-full flex justify-between absolute bottom-0">
+        <div className="w-6/12 h-auto">
+          <div className="m-auto w-9/12 mt-32">
+            <h1 className="font-bold raleway text-3xl mb-20">Hi, I am</h1>
+            <h1 className="font-bold raleway text-6xl mb-2">Abdallah Safar</h1>
+            <h1 className="font-bold raleway text-2xl text-gray-500">
+              Front-End Web Developer
+            </h1>
+          </div>
+          <div className="flex mx-auto mt-20 w-6/12">
+            <a href="mailto:abdallah.safar01@gmail.com" target="_blank">
+              <div className="w-14 h-12 bg-gray-350 shadow-md flex justify-center p-2 rounded mr-7">
+                <img src={atLogo} alt="" />
+              </div>
+            </a>
+            <a href="https://github.com/AbdallahSafar" target="_blank">
+              <div className="w-14 h-12 bg-gray-350 shadow-md flex justify-center p-2 rounded mr-7">
+                <img src={githubLogo} alt="" />
+              </div>
+            </a>
+            <a href="https://www.linkedin.com/in/abdallahsafar" target="_blank">
+              <div className="w-14 h-12 bg-gray-350 shadow-md flex justify-center p-2 rounded">
+                <img src={linkedinLogo} alt="" />
+              </div>
+            </a>
+          </div>
         </div>
-        <img
-          src={profilePic}
-          alt="Profile Picture"
-          className="h-lg absolute bottom-0 right-10"
-        />
+        <img src={profilePic} alt="Profile Picture" className="h-lg w-7/12" />
       </div>
 
-      <div className="flex ml-40 mt-10">
+      {/* <img
+          src={profilePic}
+          alt="Profile Picture"
+          className="h-lg w-6/12 absolute bottom-0 right-1/4 bg-blue-400"
+        /> */}
+
+      {/* <div className="flex ml-40 mt-10">
         <a href="mailto:abdallah.safar01@gmail.com" target="_blank">
           <div className="w-14 h-12 bg-gray-350 shadow-md flex justify-center p-2 rounded mr-7">
             <img src={atLogo} alt="" />
@@ -48,7 +70,7 @@ export default function DesktopHomePage() {
             <img src={linkedinLogo} alt="" />
           </div>
         </a>
-      </div>
+      </div> */}
     </div>
   );
 }
