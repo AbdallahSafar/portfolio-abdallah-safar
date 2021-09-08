bb3dbdcf78b346c316e0bf6d0668ceb9302e0117
Abdallah Safar <abdallah.safar01@gmail.com>
2021-09-08 22:45:35 +0300

    feat: fix design of skills component for small screens

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -9,30 +9,36 @@ export default function Skills() {
       <div className="border-4 border-black p-5 w-56 h-16 flex items-center justify-center montserrat font-semibold text-2xl mt-20">
         SKILLS
       </div>
-      <div className="w-8/12 mt-10">
-        <h1 className="montserrat text-xl font-bold ml-28">USING NOW:</h1>
+      <div className="w-8/12 mt-10 sm:w-full sm:flex sm:justify-center md:w-full md:flex md:justify-center">
+        <h1 className="montserrat text-xl font-bold ml-28 sm:ml-0 md:ml-0">
+          USING NOW:
+        </h1>
       </div>
-      <div className="flex flex-wrap w-8/12 justify-center mt-10">
+      <div className="flex flex-wrap w-8/12 justify-center mt-10 sm:w-full">
         {usingNow.map((skill) => (
           <div className="mx-12 mb-14" key={skill.name}>
             <Skill skillData={skill} />
           </div>
         ))}
       </div>
-      <div className="w-8/12 mt-14">
-        <h1 className="montserrat text-xl font-bold ml-28">LEARNING:</h1>
+      <div className="w-8/12 mt-14 sm:w-full sm:flex sm:justify-center md:w-full md:flex md:justify-center">
+        <h1 className="montserrat text-xl font-bold ml-28 sm:ml-0 md:ml-0">
+          LEARNING:
+        </h1>
       </div>
-      <div className="flex flex-wrap w-8/12 justify-center mt-10">
+      <div className="flex flex-wrap w-8/12 justify-center mt-10 sm:w-full">
         {learning.map((skill) => (
           <div className="mx-12 mb-14" key={skill.name}>
             <Skill skillData={skill} />
           </div>
         ))}
       </div>
-      <div className="w-8/12 mt-14">
-        <h1 className="montserrat text-xl font-bold ml-28">OTHER SKILLS:</h1>
+      <div className="w-8/12 mt-14 sm:w-full sm:flex sm:justify-center md:w-full md:flex md:justify-center">
+        <h1 className="montserrat text-xl font-bold ml-28 sm:ml-0 md:ml-0">
+          OTHER SKILLS:
+        </h1>
       </div>
-      <div className="flex flex-wrap w-8/12 justify-center mt-10">
+      <div className="flex flex-wrap w-8/12 justify-center mt-10 sm:w-full">
         {otherSkills.map((skill) => (
           <div className="mx-12 mb-14" key={skill.name}>
             <Skill skillData={skill} />
