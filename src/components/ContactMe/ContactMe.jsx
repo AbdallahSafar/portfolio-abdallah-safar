1858a520f6d8c4e74c42f63361070b0e21205cd2
Abdallah Safar <abdallah.safar01@gmail.com>
2021-09-27 21:40:53 +0300

    feat: add arabic language to contactme component

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import separator from "../../images/separator.svg";
 import emailjs from "emailjs-com";
 import { userID } from "../../EmailJS-userID";
+import { useContext } from "react/cjs/react.development";
+import { StateContext } from "../../StateProvider";
 
 export default function ContactMe() {
+  const [state] = useContext(StateContext);
+
   const [person, setPerson] = useState({
     name: "",
     email: "",
@@ -44,11 +48,17 @@ export default function ContactMe() {
       id="contact-me"
       className="bg-gray-250 w-full h-80 flex flex-col items-center h-auto"
     >
-      <div className="border-4 border-black p-5 w-56 h-16 flex items-center justify-center montserrat font-semibold text-2xl mt-20">
-        CONTACT
+      <div
+        className={`border-4 border-black p-5 w-56 h-16 flex items-center justify-center montserrat font-semibold text-2xl mt-20 ${
+          state.currentLanguage === "Arabic" && "text-3xl"
+        }`}
+      >
+        {state.currentLanguage === "English" ? "CONTACT" : "تواصل"}
       </div>
       <h1 className="text-2xl font-base mt-12 mb-8">
-        I would love to hear from you!
+        {state.currentLanguage === "English"
+          ? "I would love to hear from you!"
+          : "!يسعدني أن أسمع رأيك"}
       </h1>
       <img src={separator} alt="separator" />
       <form
@@ -57,45 +67,67 @@ export default function ContactMe() {
       >
         <input
           type="text"
-          placeholder="ENTER YOUR NAME*"
+          placeholder={
+            state.currentLanguage === "English"
+              ? "ENTER YOUR NAME*"
+              : "*أدخل إسمك"
+          }
           name="name"
           value={person.name}
-          className="border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-10 px-2 font-medium montserrat focus:outline-none mt-20"
+          className={`border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-10 px-2 font-medium ${
+            state.currentLanguage === "English" && "montserrat"
+          } focus:outline-none mt-20`}
           onChange={(event) => handleInputTyping("name", event.target.value)}
           required
         />
         <input
           type="email"
-          placeholder="ENTER YOUR EMAIL*"
+          placeholder={
+            state.currentLanguage === "English"
+              ? "ENTER YOUR EMAIL*"
+              : "*أدخل إيميلك"
+          }
           name="email"
           value={person.email}
-          className="border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-10 px-2 font-medium montserrat focus:outline-none mt-10"
+          className={`border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-10 px-2 font-medium ${
+            state.currentLanguage === "English" && "montserrat"
+          } focus:outline-none mt-20`}
           onChange={(event) => handleInputTyping("email", event.target.value)}
           required
         />
         <input
           type="number"
-          placeholder="PHONE NUMBER"
+          placeholder={
+            state.currentLanguage === "English" ? "PHONE NUMBER" : "رقم هاتفك"
+          }
           name="phone-number"
           value={person.phoneNumber}
-          className="border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-10 px-2 font-medium montserrat focus:outline-none mt-10"
+          className={`border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-10 px-2 font-medium ${
+            state.currentLanguage === "English" && "montserrat"
+          } focus:outline-none mt-20`}
           onChange={(event) =>
             handleInputTyping("phoneNumber", event.target.value)
           }
         />
         <textarea
           type="text"
-          placeholder="YOUR MESSAGE*"
+          placeholder={
+            state.currentLanguage === "English" ? "YOUR MESSAGE*" : "*رسالتك"
+          }
           name="message"
           value={person.message}
-          className="border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-32 px-2 font-medium montserrat focus:outline-none mt-10"
+          className={`border-l-4 border-b-4 border-black w-full placeholder-gray-600 bg-transparent h-32 px-2 font-medium ${
+            state.currentLanguage === "English" && "montserrat"
+          } focus:outline-none mt-10`}
           onChange={(event) => handleInputTyping("message", event.target.value)}
           required
         />
         <input
           type="submit"
-          className="border-l-4 border-r-4 border-black w-40 h-10 montserrat font-semibold mt-16 mb-10 text-lg bg-transparent cursor-pointer"
-          value="SUBMIT"
+          className={`border-l-4 border-r-4 border-black w-40 h-10 ${
+            state.currentLanguage === "English" && "montserrat"
+          } font-semibold mt-16 mb-10 text-lg bg-transparent cursor-pointer`}
+          value={state.currentLanguage === "English" ? "SUBMIT" : "أرسل"}
         />
       </form>
     </div>
