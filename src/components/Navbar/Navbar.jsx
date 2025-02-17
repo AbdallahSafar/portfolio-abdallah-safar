ef83e9c1707dbc1e47f5854579f752b249931b06
Abdallah Safar <abdallah.safar01@gmail.com>
2025-02-17 23:42:13 +0200

    Upgraded tailwind - Added animation to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,48 +18,56 @@ export default function Navbar() {
         onClick={showMenu}
       />
       <div className="flex justify-between items-center w-full md:w-lg md:absolute md:right-5 sm:hidden">
-        <a
-          href="#aboutme"
-          className={
-            state.currentLanguage === "English"
-              ? "text-white montserrat text-lg font-semibold"
-              : "text-white text-lg font-semibold"
-          }
-        >
-          {state.currentLanguage === "English" ? "About me" : "نبذة عني"}
-        </a>
-        <a
-          href="#skills"
-          className={
-            state.currentLanguage === "English"
-              ? "text-white montserrat text-lg font-semibold"
-              : "text-white text-lg font-semibold"
-          }
-        >
-          {state.currentLanguage === "English" ? "Skills" : "مهارات"}
-        </a>
-        <a
-          href="#projects"
-          className={
-            state.currentLanguage === "English"
-              ? "text-white montserrat text-lg font-semibold"
-              : "text-white text-lg font-semibold"
-          }
-        >
-          {state.currentLanguage === "English" ? "Projects" : "مشاريع"}
-        </a>
-        <a
-          href="https://drive.google.com/file/d/1C29xNC0kjKF4LOYj6XcI099ww-_PYxDq/view?usp=sharing"
-          target="_blank"
-          className={
-            state.currentLanguage === "English"
-              ? "text-white montserrat text-lg font-semibold"
-              : "text-white text-lg font-semibold"
-          }
-        >
-          {state.currentLanguage === "English" ? "Resume" : "سيرة ذاتية"}
-        </a>
-        <div className="bg-white rounded-full h-9 w-36 flex justify-center items-center">
+        <div className="transition-transform transform duration-500 ease-in-out hover:scale-125">
+          <a
+            href="#aboutme"
+            className={
+              state.currentLanguage === "English"
+                ? "text-white montserrat text-lg font-semibold"
+                : "text-white text-lg font-semibold"
+            }
+          >
+            {state.currentLanguage === "English" ? "About me" : "نبذة عني"}
+          </a>
+        </div>
+        <div className="transition-transform transform duration-500 ease-in-out hover:scale-125">
+          <a
+            href="#skills"
+            className={
+              state.currentLanguage === "English"
+                ? "text-white montserrat text-lg font-semibold"
+                : "text-white text-lg font-semibold"
+            }
+          >
+            {state.currentLanguage === "English" ? "Skills" : "مهارات"}
+          </a>
+        </div>
+        <div className="transition-transform transform duration-500 ease-in-out hover:scale-125">
+          <a
+            href="#projects"
+            className={
+              state.currentLanguage === "English"
+                ? "text-white montserrat text-lg font-semibold"
+                : "text-white text-lg font-semibold"
+            }
+          >
+            {state.currentLanguage === "English" ? "Projects" : "مشاريع"}
+          </a>
+        </div>
+        <div className="transition-transform transform duration-500 ease-in-out hover:scale-125">
+          <a
+            href="https://drive.google.com/file/d/1ExWnCdc_G3YwPeyMTY4nJ7wdKAlbhK4V/view?usp=drive_link"
+            target="_blank"
+            className={
+              state.currentLanguage === "English"
+                ? "text-white montserrat text-lg font-semibold"
+                : "text-white text-lg font-semibold"
+            }
+          >
+            {state.currentLanguage === "English" ? "Resume" : "سيرة ذاتية"}
+          </a>
+        </div>
+        <div className="bg-white rounded-full h-9 w-36 flex justify-center items-center transition-transform transform duration-500 ease-in-out hover:scale-125">
           <a
             href="#contact-me"
             className={
